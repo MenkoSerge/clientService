aa2d3d1c1d26b7dddf38019a3454772b120bc75f
Serge Menko <5er6emenk0@gmail.com>
2024-12-24 13:44:04 +0100

    First Initialize

---
+++ src/SaveClientOrder.java
@@ -0,0 +1,9 @@
+public class SaveClientOrder {
+    public static void main(String[] args) {
+     saveClientOrder();
+    }
+
+    static void saveClientOrder() {
+        System.out.println("Method save client order is running!");
+    }
+}
